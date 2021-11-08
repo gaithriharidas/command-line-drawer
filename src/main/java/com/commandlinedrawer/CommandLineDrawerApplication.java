865279455a29e30f65051d7a2e2b90beecb6797d
Gaithri Haridas <gaithri.haridas@versent.com.sg>
2021-11-08 09:21:28 +0800

    Initial Commit SpringBoot Application

---
+++ src/main/java/com/commandlinedrawer/CommandLineDrawerApplication.java
@@ -0,0 +1,13 @@
+package com.commandlinedrawer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandLineDrawerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommandLineDrawerApplication.class, args);
+    }
+
+}
