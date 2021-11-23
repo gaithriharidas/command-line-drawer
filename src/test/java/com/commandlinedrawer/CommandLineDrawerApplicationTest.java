0d30f069ee788e8a3b3e1113f86718ec78736bed
Gaithri Haridas <gaithri.haridas@versent.com.sg>
2021-11-23 12:23:09 +0800

    Fixed failing test cases in CommandLineDrawerApplicationTest class

--- src/test/java/com/commandlinedrawer/CommandLineDrawerApplicationTest.java
+++ src/test/java/com/commandlinedrawer/CommandLineDrawerApplicationTest.java
@@ -1,35 +1,35 @@
 package com.commandlinedrawer;
 
+import com.commandlinedrawer.app.CanvasContext;
+import com.commandlinedrawer.app.ConsoleInputSplitter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import org.mockito.Mock;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CommandLineDrawerApplicationTest {
-    private Object application;
-    private Method validateConsoleInput;
-    private Method executeCommand;
+    @Mock
+    private CanvasContext canvasContext;
+
+    @Mock
+    private ConsoleInputSplitter inputSplitter;
+
+    private CommandLineDrawerApplication app;
 
     @BeforeEach
-    public void setUp() throws IllegalAccessException, InstantiationException, NoSuchMethodException {
-        application = CommandLineDrawerApplication.class.newInstance();
-        validateConsoleInput = application.getClass().getDeclaredMethod("validateConsoleInput", String.class);
-        validateConsoleInput.setAccessible(true);
-
-        executeCommand = application.getClass().getDeclaredMethod("executeCommand", String.class);
-        executeCommand.setAccessible(true);
+    public void setUp() {
+        app = new CommandLineDrawerApplication(canvasContext, inputSplitter);
     }
 
     @Test
     public void shouldThrowExceptionWhenConsoleInputDoesntMatchPattern() {
         String errMessage = null;
         try {
-            validateConsoleInput.invoke(application, "1");
+            ReflectionTestUtils.invokeMethod(app, "validateConsoleInput", "1");
         } catch (Exception e) {
-            errMessage = ((InvocationTargetException) e).getTargetException().getMessage();
+            errMessage = e.getCause().getMessage();
         }
         assertEquals("Command string is invalid.", errMessage);
     }
@@ -38,20 +38,20 @@ public class CommandLineDrawerApplicationTest {
     public void shouldThrowExceptionWhenConsoleInputIsInvalid() {
         String errMessage = null;
         try {
-            validateConsoleInput.invoke(application, "c 15 15");
+            ReflectionTestUtils.invokeMethod(app, "validateConsoleInput", "c 15 15");
         } catch (Exception e) {
-            errMessage = ((InvocationTargetException) e).getTargetException().getMessage();
+            errMessage = e.getCause().getMessage();
         }
-        assertEquals("Command C is not a valid command.", errMessage);
+        assertEquals("Command c is not a valid command.", errMessage);
     }
 
     @Test
     public void shouldThrowExceptionIfCanvasIsNotCreatedBeforeShape() {
         String errMessage = null;
         try {
-            executeCommand.invoke(application, "R 5 8 13 15");
+            ReflectionTestUtils.invokeMethod(app, "executeCommand", "R 5 8 13 15");
         } catch (Exception e) {
-            errMessage = ((InvocationTargetException) e).getTargetException().getMessage();
+            errMessage = e.getCause().getMessage();
         }
         assertEquals("Canvas hasn't been created", errMessage);
     }
